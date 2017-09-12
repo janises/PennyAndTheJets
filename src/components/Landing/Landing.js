6ec45fd6b0ff19cd6139f6ccbabe7d6111545ae8
Janise <janises@noreply.github.com>
2017-09-12 14:23:49 -0600

    edits

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -38,7 +38,7 @@ export default class Landing extends Component {
                 {/* <div className="player" style={{"height": "40px", "width":"40px"}}></div> */}
                 {/* <div className="bird" style={{"height":"65px", "width":"50px"}}></div>
                 <div className="cloud" style={{"height":"55px", "width":"65px"}}></div>
-                <div className="plane" style={{"height":"55px", "width":"70px"}}></div>
+                
                 <div className="parachute" style={{"height":"55px", "width":"55px"}}></div> */}
                 <img className="haze" ref='haze' src={haze} alt=""/>
                 <img className="cloud1" ref="cloud1" src={cloud1} alt=''/>
@@ -47,14 +47,16 @@ export default class Landing extends Component {
                 <div className="landing-page-peng"></div>
 
                 <div className='login-container'>
-                 
+                <div className="landing-plane" style={{"height":"90px", "width":"150px"}}></div>
                    
                     <a className='login' href = 'http://localhost:8000/auth'><button className='login-button btn'>LOGIN</button></a>
                     <Link to='/instructions'> 
                         <button className='continue btn'>CONTINUE WITHOUT LOGGING IN</button>
                     </Link>
 
-                    <div className='landing-bird1'></div>
+                    <div className='landing-bird bird1'></div>
+                    <div className='landing-bird bird2'></div>
+                    <div className='landing-bird bird3'></div>
                 </div>
             </div>
             
