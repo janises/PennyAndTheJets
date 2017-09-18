9df8dabe566a483197c22a4837ad0b3cd5f8df59
Janise <janises@noreply.github.com>
2017-09-17 21:04:05 -0600

    edits

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -31,15 +31,15 @@ export default class Landing extends Component {
         //     // } else elem.css({'opacity': 0});
         //   });
 
-        const wh = $(window).height(),
-              tl = new TimelineLite({paused:true}),
-              $cloud1 = $('.cloud1'),
-              $cloud2 = $('.cloud2'),
-              $cloud3 = $('.cloud3'),
-              $haze = $('.haze'),
-              $about = $('.about'),
-              $aboutGame = $('.about-game');
-        const controller = new ScrollMagic.Controller();
+        // const wh = $(window).height(),
+        //       tl = new TimelineLite({paused:true}),
+        //       $cloud1 = $('.cloud1'),
+        //       $cloud2 = $('.cloud2'),
+        //       $cloud3 = $('.cloud3'),
+        //       $haze = $('.haze'),
+        //       $about = $('.about'),
+        //       $aboutGame = $('.about-game');
+        // const controller = new ScrollMagic.Controller();
 
         // const moveCloud1 = tl.to($cloud1, 2, {xPercent: 50});
         // const moveCloud2 = TweenLite.to($cloud2, 2, {xPercent: -50});
@@ -61,8 +61,8 @@ export default class Landing extends Component {
         //     // offset: wh
         // }).addTo(controller).setTween(moveAbout);
 
-    //     $(window).scroll(function(){
-    //         const scroll = $(this).scrollTop();
+        $(window).scroll(function(){
+            const scroll = $(this).scrollTop();
     //         console.log(scroll);
 
     //         if(scroll <= 200) {
@@ -75,15 +75,15 @@ export default class Landing extends Component {
     //         } else if(scroll <= 800 && scroll > 700) {
     //             TweenLite.to($aboutGame, 2, {xPercent: 75, yPercent: 60});
     //         }
-    //                 // $('.cloud1').css({
-    //                 //     'transform': `translate(${scroll/10}%, 0px` 
-    //                 // });
-    //                 // $('.cloud2').css({
-    //                 //     'transform': `translate(-${scroll/10}%, 0px` 
-    //                 // });
-    //                 // $('.cloud3').css({
-    //                 //     'transform': `translate(-${scroll/15}%, 0px`
-    //                 // });
+                    $('.cloud1').css({
+                        'transform': `translate(${scroll/10}%, 0px` 
+                    });
+                    $('.cloud2').css({
+                        'transform': `translate(-${scroll/10}%, 0px` 
+                    });
+                    $('.cloud3').css({
+                        'transform': `translate(-${scroll/10}%, 0px`
+                    });
     //                 $('.haze').css({
     //                     'transform': `translate(0px, -${scroll/50}%`
     //                 });
@@ -94,44 +94,44 @@ export default class Landing extends Component {
     //                 //     'transform': `translate(${scroll/18}%, ${scroll/20}%)`
     //                 // });
                 
-    //     })
+        })
     }
 
-    handleScroll(event){
-        const wh = $(window).height(),
-        // tl = new TimelineLite({paused:true}),
-        $cloud1 = $('.cloud1'),
-        $cloud2 = $('.cloud2'),
-        $cloud3 = $('.cloud3'),
-        $haze = $('.haze'),
-        $about = $('.about'),
-        $aboutGame = $('.about-game');
-        // let viewHeight = event.srcElement.body.scrollTop; //same as scrollTop
-        let scrollTop = event.target.body.scrollTop, 
-            docHeight=$(document).height(),
-            winHeight=$(window).height(),
-            scrollPercent=Math.round((scrollTop)/(docHeight-winHeight)*100);
+    // handleScroll(event){
+    //     const wh = $(window).height(),
+    //     // tl = new TimelineLite({paused:true}),
+    //     $cloud1 = $('.cloud1'),
+    //     $cloud2 = $('.cloud2'),
+    //     $cloud3 = $('.cloud3'),
+    //     $haze = $('.haze'),
+    //     $about = $('.about'),
+    //     $aboutGame = $('.about-game');
+    //     // let viewHeight = event.srcElement.body.scrollTop; //same as scrollTop
+    //     let scrollTop = event.target.body.scrollTop, 
+    //         docHeight=$(document).height(),
+    //         winHeight=$(window).height(),
+    //         scrollPercent=Math.round((scrollTop)/(docHeight-winHeight)*100);
         
-        console.log("percent", scrollPercent);
+    //     console.log("percent", scrollPercent);
 
-        if(scrollPercent <= 5){
-            TweenLite.to($cloud1, 1, {xPercent:50});
-            TweenLite.to($cloud2, 1, {xPercent:-50});
-        } else if(scrollPercent > 5 && scrollPercent <= 15){
-            TweenLite.to($about, 1.5, {xPercent:-75});
-        } else if(scrollPercent > 40 && scrollPercent <= 50) {
-            TweenLite.to($cloud3, 1, {xPercent: -50});
-        } else if(scrollPercent > 60 && scrollPercent < 80) {
-            TweenLite.to($aboutGame, 1.5, {xPercent: 75, yPercent: 60});
-        }
+    //     if(scrollPercent <= 5){
+    //         TweenLite.to($cloud1, 1, {xPercent:50});
+    //         TweenLite.to($cloud2, 1, {xPercent:-50});
+    //     } else if(scrollPercent > 5 && scrollPercent <= 15){
+    //         TweenLite.to($about, 1.5, {xPercent:-75});
+    //     } else if(scrollPercent > 40 && scrollPercent <= 50) {
+    //         TweenLite.to($cloud3, 1, {xPercent: -50});
+    //     } else if(scrollPercent > 60 && scrollPercent < 80) {
+    //         TweenLite.to($aboutGame, 1.5, {xPercent: 75, yPercent: 60});
+    //     }
   
                 
-        // console.log(viewHeight)
-    }
+    //     // console.log(viewHeight)
+    // }
 
-    componentWillUnmount(){
-        window.removeEventListener('scroll', this.handleScroll)
-    }
+    // componentWillUnmount(){
+    //     window.removeEventListener('scroll', this.handleScroll)
+    // }
 
     render(){
 
@@ -146,17 +146,17 @@ export default class Landing extends Component {
                 <div id="trigger1"></div>
                 <div className='login-container'>
                     <div className="landing-title">
-                        <h1></h1>
+                        <h1>TITLE HERE</h1>
                         {/* <img className="title-logo" src={gameLogo2} alt="game-logo"/> */}
                     </div>
                    
                     <div className="about">
-                        <h1>PENGUIN</h1>
-                        <p>Half-giant jinxes peg-leg gillywater broken glasses large black dog Great Hall. Nearly-Headless Nick now string them together, and answer me this, which creature would you be unwilling to kiss? Poltergeist sticking charm, troll umbrella stand flying cars golden locket Lily Potter. Pumpkin juice Trevor wave your wand out glass orbs, a Grim knitted hats. Stan Shunpike doe patronus, suck his soul Muggle-Born large order of drills the trace. Bred in captivity fell through the veil, quaffle blue flame ickle diddykins Aragog. Yer a wizard, Harry Doxycide the woes of Mrs. Weasley Goblet of Fire.</p>
+                        <h1>PENNY</h1>
+                        <p>Penny is tired of society telling her that she can't fly. As a strong, independent penguin, she won't let the patriarchy tell her what to do. She loves to skydive, and she often waits until the last second before she deploys her parachute to get maximum flight time.</p>
                     </div>
                     <div className="about-game">
                         <h1>GAME</h1>
-                        <p>Half-giant jinxes peg-leg gillywater broken glasses large black dog Great Hall. Nearly-Headless Nick now string them together, and answer me this, which creature would you be unwilling to kiss? Poltergeist sticking charm, troll umbrella stand flying cars golden locket Lily Potter. Pumpkin juice Trevor wave your wand out glass orbs, a Grim knitted hats. Stan Shunpike doe patronus, suck his soul Muggle-Born large order of drills the trace. Bred in captivity fell through the veil, quaffle blue flame ickle diddykins Aragog. Yer a wizard, Harry Doxycide the woes of Mrs. Weasley Goblet of Fire.</p>
+                        <p>The planes and other birds flaunt their aerial privileges and try to stop her descent. Use the arrow keys to move Penny left and right so she can continue her flight. Collect the clouds and mini-parachutes for bonus points!</p>
                     </div>
                     <div className="button-container">
                         <a className='login' href = 'http://localhost:8000/auth'><button className='login-button btn'>LOGIN</button></a>
